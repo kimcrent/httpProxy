a18f7a64b77245cf8a28a8cf9dfab5d0ea0fc20f
Roman Kim <kimcrent@gmail.com>
2025-02-03 13:41:43 +0300

    add listner

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"log"
+	"net"
+)
+
+func listner() {
+	conn, err := net.Listen("tcp", "195.96.147.217:64902")
+	if err != nil {
+		log.Fatalf("Ошибка при запуске сервера: %v", err)
+	}
+	defer conn.Close()
+
+}
+
 func main() {
+	listner()
 
 }
